f88d3292759644e7f12c04c59a3ac776f22184ce
abbazsrmit <s4068375@student.rmit.edu.au>
2025-07-19 11:50:04 +1000

    pushing all code

---
+++ swear-web/src/app/signin/page.tsx
@@ -0,0 +1,13 @@
+const SignInPage = () => {
+  return (
+    <div>
+      <h1>Sign In</h1> 
+      <h1>Email</h1>
+      <input type="email" placeholder="Enter your email" />
+      <h1>Password</h1>
+      <input type="password" placeholder="Enter your password" />
+    </div>
+  );
+}
+
+export default SignInPage;
---
+++ swear-web/src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+const SignUpPage = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1> 
+      <h1>Email</h1>
+      <input type="email" placeholder="Enter your email" />
+      <h1>Password</h1>
+      <input type="password" placeholder="Enter your password" />       
+    </div>
+  );
+}
+
+export default SignUpPage;
