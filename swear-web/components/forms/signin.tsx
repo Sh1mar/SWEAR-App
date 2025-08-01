5f153d674de315aa1e8d6d644dc92aef030298f7
Sh1mar <shimar1167@gmail.com>
2025-08-01 15:28:16 +1000

    Changed .env file to admin privelages

---
+++ swear-web/components/forms/signin.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "../ui/label"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+export default function SignInUser(){ 
+   return (
+      <>
+         <div className="flex justify-center">
+            <Card>
+               <H1> Sigin Card</H1>
+            </Card>
+         </div>
+      
+      </>
+   )
+}
